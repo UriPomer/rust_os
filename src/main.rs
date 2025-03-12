6fd4ba573742e719156f027a48e4c29d3eb2f9b2
Urim_XXL <3051068921@qq.com>
2025-03-12 20:04:10 +0800

    VGA print

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-/*
- * @Author: Urim_XXL 3051068921@qq.com
- * @Date: 2025-03-12 00:30:41
- * @LastEditors: Urim_XXL 3051068921@qq.com
- * @LastEditTime: 2025-03-12 17:00:56
- * @FilePath: /rust_os/src/main.rs
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #![no_std]
 #![no_main]
 
@@ -13,22 +5,18 @@ use core::panic::PanicInfo;
 
 /// 当 panic 发生时调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 static HELLO: &[u8] = b"Hello World!";
 
+mod vga_buffer;
+
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
